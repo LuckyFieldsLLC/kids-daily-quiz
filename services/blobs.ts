4dfa17787b6205883d43c4f9d8b9b28b0224ff45
LuckyFields.LLC <clystalblue77@gmail.com>
2025-09-23 15:50:57 +0900

    Update blobs.ts

--- services/blobs.ts
+++ services/blobs.ts
@@ -1,12 +1,12 @@
-import { createClient } from "@netlify/blobs";
+import { getStore } from "@netlify/blobs";
 
 // "quizzes" ストアに接続
-const client = createClient({ store: "quizzes" });
+const store = getStore("quizzes");
 
 export async function saveQuiz(id: string, data: object) {
-  await client.setJSON(id, data);
+  await store.setJSON(id, data);
 }
 
 export async function getQuiz(id: string) {
-  return await client.getJSON(id);
+  return await store.get(id, { type: "json" });
 }
